301a15387849119269e3b75f8b441654af7a1766
Mariya_Suchkova <mashakraxmal@gmail.com>
2026-01-15 23:47:43 +0300

    add: property in Product setter in Product .new_product() - classmethod in Product property in Category .add_product() in Category tests in new methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,15 @@ def product_2():
     return Product("Клубника", "Ягода", 201.9, 5)
 
 
+# @pytest.fixture
+# def product_3():
+#     return Product("Малина", "Ягода", 159.1, 15)
+#
+# @pytest.fixture
+# def product_4():
+#     return Product("Голубика", "Ягода", 267.7, 3)
+
+
 @pytest.fixture
 def category_1():
     return Category("Сладости", "Для детей", ["Яблочное пюре", "Запеканка", "Сок", "Мороженное"])
@@ -21,3 +30,15 @@ def category_1():
 @pytest.fixture
 def category_2():
     return Category("Овощи", "ЗОЖ", ["Помидор", "Кабачок", "Тыква"])
+
+
+@pytest.fixture
+def category_3():
+    return Category(
+        "Ягоды", "Летний сезон", [Product("Малина", "Ягода", 159.1, 15), Product("Голубика", "Ягода", 267.7, 3)]
+    )
+
+
+@pytest.fixture
+def product_dict_1():
+    return {"name": "Ежевика", "description": "Ягода", "price": 211.4, "quantity": 7}
--- tests/test_creating_classes.py
+++ tests/test_creating_classes.py
@@ -1,4 +1,4 @@
-from src.creating_classes import Category
+from src.creating_classes import Category, Product
 
 
 def test_init_product_1(product_1):
@@ -18,7 +18,7 @@ def test_init_product_2(product_2):
 def test_init_category_1(category_1):
     assert category_1.name == "Сладости"
     assert category_1.description == "Для детей"
-    assert category_1.products == ["Яблочное пюре", "Запеканка", "Сок", "Мороженное"]
+    assert category_1.products_in_list == ["Яблочное пюре", "Запеканка", "Сок", "Мороженное"]
     assert Category.category_count == 1
     assert Category.product_count == 4
 
@@ -26,6 +26,38 @@ def test_init_category_1(category_1):
 def test_init_category_2(category_1, category_2):
     assert category_2.name == "Овощи"
     assert category_2.description == "ЗОЖ"
-    assert category_2.products == ["Помидор", "Кабачок", "Тыква"]
+    assert category_2.products_in_list == ["Помидор", "Кабачок", "Тыква"]
     assert Category.category_count == 3
     assert Category.product_count == 3
+
+
+def test_setter_product_1(product_1):
+    product_1.price = 15
+    assert product_1.price == 15
+
+
+def test_setter_product_2(capsys, product_1):
+    product_1.price = -3
+    captured = capsys.readouterr()
+    assert captured.out == "Цена не должна быть нулевая или отрицательная\n"
+
+
+def test_new_product(product_dict_1):
+    assert Product.new_product(product_dict_1).name == "Ежевика"
+    assert Product.new_product(product_dict_1).description == "Ягода"
+    assert Product.new_product(product_dict_1).price == 211.4
+    assert Product.new_product(product_dict_1).quantity == 7
+
+
+def test_property_category(category_3):
+    assert category_3.products == "Малина, 159.1 руб. Остаток: 15 шт.\nГолубика, 267.7 руб. Остаток: 3 шт.\n"
+
+
+def test_add_product_category(category_3, product_2):
+    category_3.add_product(product_2)
+    assert category_3.products == (
+        "Малина, 159.1 руб. Остаток: 15 шт.\n"
+        "Голубика, 267.7 руб. Остаток: 3 шт.\n"
+        "Клубника, 201.9 руб. Остаток: 5 шт.\n"
+    )
+    assert category_3.product_count == 3
