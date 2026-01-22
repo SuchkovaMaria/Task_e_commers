b288487468deaeb12adfb0b7b3cc07d51b0d3930
Mariya_Suchkova <mashakraxmal@gmail.com>
2026-01-22 22:13:34 +0300

    add docstrings in metods

--- src/creating_classes.py
+++ src/creating_classes.py
@@ -10,15 +10,18 @@ class Product:
     quantity: int
 
     def __init__(self, name, description, price, quantity):
+        """Конструктор экземпляра класса Product"""
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
     def __str__(self):
+        """Метод вывода данных продукта"""
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
+        """Метод сложения цен двух продуктов (каждая цена умножена на количество шт. на складе)"""
         if isinstance(other, Product):
             return self.__price * self.quantity + other.__price * other.quantity
         else:
@@ -26,27 +29,21 @@ class Product:
 
     @property
     def price(self):
+        """Вывод цены продукта"""
         return self.__price
 
     @price.setter
     def price(self, price):
+        """Изменение цены продукта"""
         if price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
         else:
             self.__price = price
 
     @classmethod
-    def new_product(cls, propuct):
-        for k, v in propuct.items():
-            if k == "name":
-                name = v
-            elif k == "description":
-                description = v
-            elif k == "price":
-                price = v
-            elif k == "quantity":
-                quantity = v
-        return cls(name, description, price, quantity)
+    def new_product(cls, product):
+        """Метод для создания продукта"""
+        return cls(product["name"], product["description"], product["price"], product["quantity"])
 
 
 class Category:
@@ -60,6 +57,7 @@ class Category:
     product_count = 0
 
     def __init__(self, name, description, products):
+        """Конструктор экземпляра класса Category"""
         self.name = name
         self.description = description
         self.__products = products if products else []
@@ -68,6 +66,7 @@ class Category:
         Category.product_count = len(self.__products)
 
     def __str__(self):
+        """Вывод информации по категории"""
         product_count = []
         for product in self.__products:
             product_count.append(product.quantity)
@@ -75,6 +74,7 @@ class Category:
 
     @property
     def products(self):
+        """Вывод списка пропуктов в категории (строкой)"""
         products_str = ""
         for product in self.__products:
             products_str += f"{str(product)}\n"
@@ -82,6 +82,7 @@ class Category:
 
     @property
     def products_in_list(self):
+        """Вывод списка продуктов в категории (list)"""
         return self.__products
 
     def add_product(self, product: Product):
--- tests/test_creating_classes.py
+++ tests/test_creating_classes.py
@@ -75,7 +75,8 @@ def test_product_add(product_1, product_2):
 
 def test_product_add_error(product_1, category_3):
     with pytest.raises(TypeError):
-        product_1 + category_3
+        i = product_1 + category_3
+
 
 def test_category_str(category_3):
     assert str(category_3) == "Ягоды, количество продуктов: 18 шт."
